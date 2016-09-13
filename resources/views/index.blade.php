dd503facfa4c514ae79976b5e3120dc2fa91e030
gluten <goulvenschaal@me.com>
2016-09-13 14:57:50 +0200

    Séquence 2 complétée

---
+++ resources/views/index.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Plateforme de microblogging">
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" media="screen" title="no title">
+
+    <title>Twitter</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <h1>Dernier tweets</h1>
+
+            <div class="col-md-6">
+
+                @foreach($tweets as $tweet)
+                    <div class="media well">
+                        <a class="media-left" href="#">
+                            <!-- Photo de profil -->
+                        </a>
+                        <div class="media-body">
+                            <h4 class="media-heading">@??<small class="pull-right text-muted">{{ $tweet->created_at->diffForHumans() }}</small></h4>
+                            {{ $tweet->content }}
+                        </div>
+                    </div>
+                @endforeach
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,20 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    $tweets = App\Tweet::all();
+
+    return view('index', ['tweets' => $tweets]);
+});
+
+Route::get('/a-propos', function() {
+    return 'Ce clone de Twitter vous est proposé par Laravel et Open School Design';
+});
+
+Route::get('/contact', function() {
+    return 'Écrivez nous à exemple@exemple.org';
+});
+
+Route::post('/contact', function() {
+    return 'Désolé le formulaire de contact n’est pas encore prêt';
 });
